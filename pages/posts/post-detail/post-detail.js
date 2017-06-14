6e66c2c61d271aee9d1c34f563a81c1c4217b2af
newhuaszh <szhhzs@sina.com>
2017-06-14 22:59:06 +0800

    2017-6-14 22:58:58

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,74 +1,65 @@
-var post_data = require("../../data/posts-data.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  openDetail: function (event) {
-    wx.navigateTo({
-      url: 'post-detail/post-detail'
-    })
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      postList: post_data.postList
-    });
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log("onready")
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log("onshow")
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log("onhide")
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log("onunload")
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
